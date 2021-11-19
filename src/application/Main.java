c006409c8139d8979e7a217e6d801f1830efad73
Flynn <fpyc@wgu.edu>
2021-11-19 22:34:15 +0000

    Updated .gitignore

---
+++ src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    /**
+     * Default application point of execution.
+     * @param args the supplied command-line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
