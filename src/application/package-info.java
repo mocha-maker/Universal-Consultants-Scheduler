ad4416003b346e1e21986e094a908ba7e1690cf8
Mocha <69407485+mocha-maker@users.noreply.github.com>
2022-03-21 12:41:34 -0400

    Update package-info.java
    Removed wgu reference

--- src/application/package-info.java
+++ src/application/package-info.java
@@ -3,8 +3,6 @@
  * This is a scheduling application for a business managing clients from various timezones.
  * It tracks customers and appointments through interfaces and reports.
  *
- * <b> Courser</b>: C195 - Software II (Java)
- * <b>Date of Submission:</b> January 31, 2022
  * @since 1.0
  * @author Flynn Pyc (#1221251)
  * @version 0.1
